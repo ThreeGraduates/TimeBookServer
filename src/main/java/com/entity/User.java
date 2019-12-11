d0567d40e12a3c62d62fea8d383892ac9ecda320
liumengqi1 <3112313360@qq.com>
2019-12-11 11:48:54 +0800

    引入@Data注解

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -1,31 +1,21 @@
 package com.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "t_user")
+@Data  //@Data包含了所有的get、set、和toString方法，为避免循环引用报错，可以再重写一下toString方法
+@NoArgsConstructor  //相当于加入无参构造函数
 public class User {
 
+    @Id
     private String name;
     private Integer age;
-    @Id
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
